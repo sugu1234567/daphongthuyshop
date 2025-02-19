6e549604eb6bb767f1784b3f43d524822a560dab
Nam <ngovannam10022003@gmail.com>
2025-02-19 13:56:52 +0700

    init

---
+++ src/test/java/vn/sugu/daphongthuyshop/DaphongthuyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.sugu.daphongthuyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaphongthuyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
